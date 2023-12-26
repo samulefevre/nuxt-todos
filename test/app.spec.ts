ce030ee72f48c667e61f32db490122cd0af99726
Samuel LEFEVRE <samuel.lefevr@gmail.com>
2023-12-26 22:23:12 +0100

    Refactor app.spec.ts

--- test/app.spec.ts
+++ test/app.spec.ts
@@ -1,22 +1,22 @@
-import { describe, expect, test, beforeEach } from 'vitest';
-import { VueWrapper, shallowMount } from '@vue/test-utils';
+import { describe, expect, it, beforeEach } from 'vitest'
+import { VueWrapper, shallowMount } from '@vue/test-utils'
 
-import App from '@/app.vue';
+import App from '@/app.vue'
 
-import { UNotifications, NuxtPage } from '#components';
+import { UNotifications, NuxtPage } from '#components'
 
 describe('test app.vue', () => {
-    let wrapper: VueWrapper;
+    let wrapper: VueWrapper
 
     beforeEach(() => {
-        wrapper = shallowMount(App);
+        wrapper = shallowMount(App)
     })
 
-    test('app.vue should contain NuxtPage', () => {
-        expect(wrapper.findComponent(NuxtPage).exists()).toBe(true);
+    it('should contain NuxtPage', () => {
+        expect(wrapper.findComponent(NuxtPage).exists()).toBe(true)
     })
 
-    test('app.vue should contain UNotifications', () => {
-        expect(wrapper.findComponent(UNotifications).exists()).toBe(true);
+    it('should contain UNotifications', () => {
+        expect(wrapper.findComponent(UNotifications).exists()).toBe(true)
     })
-});
+})
